89ac4ce161860aa3fd380b9c2dcb18a90598ca4a
wenzheng <1584829864@qq.com>
2020-09-08 18:17:50 +0800

    更新

--- scripts/get_info.py
+++ scripts/get_info.py
@@ -20,37 +20,7 @@ formatter = logging.Formatter(
             '%(asctime)s [%(levelname)s] [%(module)s:%(funcName)s]- %(message)s [%(module)s:%(funcName)s]')
 Rthandler.setFormatter(formatter)
 logging.getLogger('').addHandler(Rthandler)
-# sh = logging.StreamHandler()#往屏幕上输出
-# sh.setFormatter(formatter) #设置屏幕上显示的格式
 
-# mutex = threading.Lock()
-# def setLogger(log):
-#     global logger
-#     logger = log
-#
-#
-# def set_ser_log(log):
-#     global ser_logger
-#     ser_logger = log
-
-# def getLogger(type='none',tag=''):
-#     log = None
-#     if type == 'ser':
-#         if mutex.acquire():
-#             if not ser_logger:
-#                 log = logging
-#             else:
-#                 log = ser_logger
-#             mutex.release()
-#         return log
-#     else:
-#         if mutex.acquire():
-#             if not logger:
-#                 log = logging
-#             else:
-#                 log = logger
-#             mutex.release()
-#         return log
 
 try:
     starttime = datetime.datetime.now()
@@ -89,12 +59,17 @@ try:
     more_job_detail = list(set(more_job_detail))
     dataurlSum = dataurl+more_job_detail
     dataurlSum = list(set(dataurlSum))
-    job_special2 = []
     job_end1= datetime.datetime.now()
-    getLogger().info('寻找链接'+url2+'的总时间为'+str((starttime - job_end1).seconds)+'s')
+    getLogger().info('寻找链接' + url2 +',链接总数' + str(len(dataurlSum)) + ',总时间为' + str((job_end1 - starttime).seconds) + 's')
     for dataurlDetail in dataurlSum:
+        job_special2 = []
         sync_start2 = datetime.datetime.now()
+        # getLogger().info(dataurlDetail)
         r_detial=requests.get(dataurlDetail)
+        if r_detial.reason.lower() !='ok':
+            job_information2[int(dataurlSum.index(dataurlDetail)+1)]={'job_desc':'网址打开失败，返回'+str(r_detial.reason.lower())+'状态码为'+str(r_detial.status_code),'job_url':dataurlDetail}
+            getLogger().error('网址'+str(dataurlDetail)+'打开失败，返回'+str(r_detial.reason.lower())+'状态码为'+str(r_detial.status_code))
+            continue
         soupDetail = bs4.BeautifulSoup(r_detial.text,'lxml')
         urlDe = (soupDetail.find('section',attrs={'class':'job-details-page'})).find('div',attrs={'class':'wrapper'})
         job_summary =(urlDe.find('div',attrs={'class':'content'}))
@@ -115,9 +90,17 @@ try:
         job_desc=job_summary.find('div',attrs={'class':'job-description'}).text.strip()
         job_apply=url2+((job_summary.find('div',attrs={'class':'apply-wrapper'})).find('a',attrs={'class':'btn btn-apply'})).attrs['href']
         sync_stop2 = datetime.datetime.now()
-        job_information2[int(dataurlSum.index(dataurlDetail)+1)]={'job_name':job_name_2,'job_company':job_company,'job_time':job_time,'job_special':job_special2,'job_desc':job_desc,'job_apply':job_apply}
-        getLogger().info('解析'+url2+'的序号为'+str(dataurlSum.index(dataurlDetail)+1)+'的url的时间为'+str((sync_stop2 - sync_start2).seconds)+'s')
-    index_start = len(job_information2)+1
+        job_information2[int(dataurlSum.index(dataurlDetail)+1)]={'job_name':job_name_2,'job_company':job_company,'job_time':job_time,'job_special':job_special2,'job_desc':job_desc,'job_apply':job_apply,'job_url':dataurlDetail}
+        getLogger().info('解析序号为'+str(dataurlSum.index(dataurlDetail)+1)+'的url:'+dataurlDetail+'的时间为'+str((sync_stop2 - sync_start2).seconds)+'s')
+    endtime_url2=datetime.datetime.now()
+    getLogger().info('测试' + url2 + ',链接总数为:'+str(len(dataurlSum))+',总时间为:' + str((endtime_url2 - starttime).seconds) + 's,大概'+str('%.2f' % float(((endtime_url2 - starttime).seconds)/60.0))+'min')
+    #将爬虫信息写入json文件
+    json_dicts2 = json.dumps(job_information2, indent=4, ensure_ascii=False)
+    with open('D:\work\\factory\杂\\RemotiveGetInfo.json', 'w', encoding="UTF-8") as f:
+        f.write(json_dicts2)
+
+    starttime_url1=datetime.datetime.now()
+    # index_start = len(job_information2)+1
     url1="https://weworkremotely.com"
     find_http_start = datetime.datetime.now()
     r = requests.get(url1)
@@ -130,7 +113,7 @@ try:
     job_special = []
 
     job_information ={}
-
+    # job_information.update(job_information2)
     def getJobLink(tag_tmp):
         jobLink_tmp =[]
         jobcategry_tmp=[]
@@ -151,8 +134,8 @@ try:
         return list(set(jobLink_tmp)),list(set(jobcategry_tmp))
 
     jobLink,jobcategry = getJobLink(tag)
-    getLogger().info(len(jobcategry))
-    getLogger().info(len(jobLink))
+    # getLogger().info(len(jobcategry))
+    # getLogger().info(len(jobLink))
 
     jobLink_category = []
     for link in jobcategry:
@@ -162,36 +145,31 @@ try:
         tag_link = (soup_link.find('div', attrs={'id': 'job_list'})).find_all('a')
         jobLink_category_2,jobcategry_2 = getJobLink(tag_link)
         jobLink_category= jobLink_category+jobLink_category_2
-        print(jobcategry.index(link))
-        # for tagi_link in tag_link:
-        #     if 'href' in tagi_link.attrs.keys():
-        #         contenets = tagi_link.contents
-        #         for i in contenets:
-        #             if isinstance(i,bs4.element.Tag):
-        #                 list_value = list(i.attrs.values())[:]
-        #                 if ['company'] in list_value:
-        #                     jobLink_category.append(url1+tagi_link.attrs['href'])
-        #             else:
-        #                 continue
-    getLogger().info(len(jobLink_category))
+        # print(jobcategry.index(link))
+
+    # getLogger().info(len(jobLink_category))
     
     jobLink_sum = jobLink+jobLink_category
     jobLink_sum = list(set(jobLink_sum))
-    getLogger().info(len(jobLink_sum))
+    # getLogger().info(len(jobLink_sum))
 
     find_http_stop = datetime.datetime.now()
-    getLogger().info('寻找链接'+str(url1)+'的总时间为'+str((find_http_stop - find_http_start).seconds)+'s')
+    getLogger().info('寻找链接'+str(url1)+',链接总数'+str(len(jobLink_sum))+',的总时间为'+str((find_http_stop - find_http_start).seconds)+'s')
 
     for jj in jobLink_sum:
         link_analysis_start= datetime.datetime.now()
         job_special=[]
         r1 = requests.get(jj)
-        getLogger().info(index_start)
-        getLogger().info(jj)
+        if r_detial.reason.lower() != 'ok':
+            job_information[int(jobLink_sum.index(jj))+1] = {'job_desc': '网址打开失败,返回值为'+str(r_detial.reason.lower())+'状态码'+str(r_detial.status_code),  'job_link': jj}
+            getLogger().error('网址：'+jj+'打开失败,返回值为'+str(r_detial.reason.lower())+'状态码'+str(r_detial.status_code))
+            # index_start = index_start + 1
+            continue
+        # getLogger().info(index_start)
+        # getLogger().info(jj)
         soup1 = bs4.BeautifulSoup(r1.text,'lxml')
         job_sum1 = soup1.find('section',attrs={'id':'job-show'})
         job_desc = ((job_sum1.find('div',attrs={'id':'job-listing-show-container'})).contents)
-            # .text.strip()
         job_desc = map(str,job_desc)
         job_desc = ''.join(job_desc)
         job_applyLink= (job_sum1.find('a',attrs={'id':'job-cta-alt'})).attrs['href']
@@ -202,22 +180,22 @@ try:
         job_special_tmp = job_sum2.find_all('span',attrs={'class':'listing-tag'})
         for i in job_special_tmp:
             job_special.append(i.text.strip())
-        job_information[int(index_start)] = {'job_desc':job_desc,'job_time':job_time,'job_name':job_name,'job_special':job_special,'job_link':jj,'job_applyLink':job_applyLink}
+        job_information[int(jobLink_sum.index(jj))+1] = {'job_desc':job_desc,'job_time':job_time,'job_name':job_name,'job_special':job_special,'job_link':jj,'job_applyLink':job_applyLink}
         link_analysis_stop = datetime.datetime.now()
-        getLogger().info('序号'+str(index_start)+'的链接解析需要的时间为' + str((link_analysis_stop - link_analysis_start).seconds)+'s')
-        index_start=index_start+1
+        getLogger().info('解析序号为'+str(int(jobLink_sum.index(jj))+1)+'的url:'+jj+'的时间为' + str((link_analysis_stop - link_analysis_start).seconds)+'s')
+        # index_start=index_start+1
     # getLogger().info(job_desc)
     # getLogger().info(job_time)
     # getLogger().info(job_name)
     # getLogger().info(job_special)
     # getLogger().info(job_information)
     json_dicts = json.dumps(job_information, indent=4, ensure_ascii=False)
-    # log_result_file = open('D:\work\\factory\杂\get_info.json', 'a',encoding="UTF-8")
-    with open('D:\work\\factory\杂\get_info.json', 'w',encoding="UTF-8") as f:
+    with open('D:\work\\factory\杂\WeworkremotelyGetInfo.json', 'w',encoding="UTF-8") as f:
         f.write(json_dicts)
     # log_result_file.write(json_dicts)
     # log_result_file.close()
     endtime = datetime.datetime.now()
-    getLogger().info('测试总时间为:'+str((endtime - starttime).seconds)+'s')
+    getLogger().info('测试' + url1 +',链接总数：' + str(len(jobLink_sum)) + ',总时间为:' + str((endtime - starttime_url1).seconds) + 's,大概'+str('%.2f' % float(((endtime - starttime_url1).seconds)/60.0))+'min')
+    getLogger().info('爬虫结束，测试总时间为：'+str((endtime-starttime).seconds)+'s,大概'+str('%.2f' % float(((endtime - starttime).seconds)/60.0))+'min')
 except Exception as e:
     getLogger().exception(e)
